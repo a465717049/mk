440387952204a30906e66dd159adbca49419df35
a465717049 <465717049@qq.com>
2021-01-04 09:43:46 +0800

    等级修改

--- mkadmin/src/views/Player/PlayerLevel.vue
+++ mkadmin/src/views/Player/PlayerLevel.vue
@@ -15,11 +15,9 @@
       </el-table-column>
       <el-table-column prop="create_time" label="注册日期" width="150" sortable>
       </el-table-column>
-      <el-table-column prop="ison" :formatter="formatSon" label="主/子" width="" sortable>
+      <el-table-column prop="lv_name" :formatter="formatlv" label="职位" width="" sortable>
       </el-table-column>
-      <el-table-column prop="lv_name" label="荣誉" width="" sortable>
-      </el-table-column>
-      <el-table-column prop="farmers" :formatter="formatfarmers" label="三星/二星" width="130" sortable>
+      <el-table-column prop="farmers" :formatter="formatfarmers" label="等级" width="130" sortable>
       </el-table-column>
       <el-table-column prop="tid" label="推荐ID" width="100" sortable>
       </el-table-column>
@@ -39,10 +37,24 @@
     </el-col>
 
     <!--荣誉界面-->
-    <el-dialog title="更改荣誉" :visible.sync="levelFormVisible" v-model="levelFormVisible" :close-on-click-modal="false">
+    <el-dialog title="更改级别" :visible.sync="levelFormVisible" v-model="levelFormVisible" :close-on-click-modal="false">
       <el-form :model="Formlevel" label-width="80px" :rules="Formlevels" ref="Formlevel">
-        <el-form-item label="荣誉" prop="Name">
-          <el-input v-model="Formlevel.Name" auto-complete="off"></el-input>
+        <el-form-item label="职位" prop="Name">
+          <el-select v-model="Formlevel.Name" placeholder="请选择职位">
+          <el-option label="会员" :value="0"></el-option>
+          <el-option label="经理" :value="1"></el-option>
+          <el-option label="总监" :value="2"></el-option>
+          <el-option label="总裁" :value="3"></el-option>
+          <el-option label="董事" :value="4"></el-option>
+          <el-option label="合伙人" :value="5"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="等级" >
+        <el-select v-model="Formlevel.honur" placeholder="请选择等级">
+        <el-option label="初级会员" :value="1"></el-option>
+        <el-option label="中级会员" :value="2"></el-option>
+        <el-option label="高级会员" :value="3"></el-option>
+         </el-select>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -91,11 +103,12 @@ export default {
       levelFormVisible: false,
       levelLoading: false,
       Formlevels: {
-        Name: [{ required: true, message: "请输入荣誉", trigger: "blur" }],
+        Name: [{ required: true, message: "请输入职位", trigger: "blur" }],
       },
       Formlevel: {
         Id: 0,
         Name: "",
+        honur:"",
       },
       users: [],
       roles: [],
@@ -144,18 +157,29 @@ export default {
         });
         return;
       }
+      
       this.Formlevel.Id = rows.uid;
+      this.Formlevel.Name = rows.lv_name;
+      console.log(rows.farmers)
+      this.Formlevels.honur=rows.farmers;
       this.levelFormVisible = true;
     },
     selectCurrentRow(val) {
       this.currentRow = val;
     },
 
-    formatSon: function (row, column) {
-      return row.ison == 1 ? "子" : row.ison == 0 ? "主" : "未知";
+    formatlv: function (row, column) {
+          var tmpname="";
+          if (row.lv_name == 0) tmpname= '会员'
+          if (row.lv_name == 1) tmpname= '经理'
+          if (row.lv_name == 2) tmpname= '总监'
+          if (row.lv_name == 3) tmpname= '总裁'
+          if (row.lv_name == 4) tmpname= '董事'
+          if (row.lv_name == 5) tmpname= '合伙人'
+      return tmpname;
     },
     formatfarmers: function (row, column) {
-      return row.farmers == 1 ? "三星" : row.farmers == 0 ? "二星" : "未知";
+      return row.farmers == 1 ? "初级会员" : row.farmers == 2 ? "中级会员" : "高级会员";
     },
     callFunction(item) {
       this.filters = {
